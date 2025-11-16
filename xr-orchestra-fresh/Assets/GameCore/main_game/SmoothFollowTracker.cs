eff0777df3cff904aa61ca26b2721b34b6dd4dc8
oyshoboy <oyshoboy@gmail.com>
2025-11-16 04:52:04 -0800

    the dummies are suppose to be working

--- xr-orchestra-fresh/Assets/GameCore/main_game/SmoothFollowTracker.cs
+++ xr-orchestra-fresh/Assets/GameCore/main_game/SmoothFollowTracker.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class SmoothFollowTracker : MonoBehaviour
 {
+    public enum AxisLock
+    {
+        None,
+        X,
+        Y,
+        Z
+    }
+
     [SerializeField] private Transform target;
     
     [SerializeField] private float positionSmoothness = 8f;
     [SerializeField] private float rotationSmoothness = 6f;
+    [SerializeField] private AxisLock lockedRotationAxis = AxisLock.None;
 
     private void LateUpdate()
     {
@@ -24,7 +33,38 @@ public class SmoothFollowTracker : MonoBehaviour
     private void SmoothFollowRotation()
     {
         float rotationBlend = 1f - Mathf.Exp(-rotationSmoothness * Time.deltaTime);
+
+        if (lockedRotationAxis == AxisLock.None)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationBlend);
+            return;
+        }
+
+        Vector3 currentEuler = transform.localEulerAngles;
+        float lockedAngle = lockedRotationAxis switch
+        {
+            AxisLock.X => currentEuler.x,
+            AxisLock.Y => currentEuler.y,
+            AxisLock.Z => currentEuler.z,
+            _ => 0f
+        };
+
         transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationBlend);
+
+        Vector3 newEuler = transform.localEulerAngles;
+        switch (lockedRotationAxis)
+        {
+            case AxisLock.X:
+                newEuler.x = lockedAngle;
+                break;
+            case AxisLock.Y:
+                newEuler.y = lockedAngle;
+                break;
+            case AxisLock.Z:
+                newEuler.z = lockedAngle;
+                break;
+        }
+        transform.localEulerAngles = newEuler;
     }
 
     public void SetTarget(Transform newTarget)
--- xr-orchestra-fresh/Assets/GameCore/main_game/SmoothGhostInitializer.cs
+++ xr-orchestra-fresh/Assets/GameCore/main_game/SmoothGhostInitializer.cs
@@ -5,11 +5,14 @@ public class SmoothGhostInitializer : MonoBehaviour
     [SerializeField] private SmoothFollowTracker smoothHeadTracker;
     [SerializeField] private SmoothFollowTracker smoothLeftHandTracker;
     [SerializeField] private SmoothFollowTracker smoothRightHandTracker;
+    [SerializeField] private SmoothFollowTracker smoothBodyTracker;
 
     public void Initialize(Transform headTarget, Transform leftHandTarget, Transform rightHandTarget)
     {
-        if (smoothHeadTracker != null && headTarget != null)
+        if (smoothHeadTracker != null && headTarget != null){
             smoothHeadTracker.SetTarget(headTarget);
+            smoothBodyTracker.SetTarget(headTarget);
+        }
         
         if (smoothLeftHandTracker != null && leftHandTarget != null)
             smoothLeftHandTracker.SetTarget(leftHandTarget);
